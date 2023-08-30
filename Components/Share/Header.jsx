d67d1f10d24a22e58db1c4e475aff3185b6b7bfa
priyans619 <priyansh.2b@gmail.com>
2023-08-30 21:33:48 +0530

    Adding Header with Dark mode functionality

---
+++ Components/Share/Header.jsx
@@ -0,0 +1,42 @@
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import styles from "styles/index.module.scss";
+import { motion } from "framer-motion";
+import { useGlobalContext } from "../AppProvider";
+
+export default function Header() {
+  const { setDarkTheme, darkTheme } = useGlobalContext();
+
+  const handleToggle = () => {
+    setDarkTheme(!darkTheme);
+  };
+
+  return (
+    <Row
+      className={`${styles.h70px} header px-3 align-content-center fixed-top shadow-sm`}
+    >
+      <Col>
+        <motion.p
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
+          className=" fw-bold fs-5"
+        >
+          
+        </motion.p>
+      </Col>
+      <Col xs="auto">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.8 }}
+          whileTap={{ rotate: 270 }}
+          onClick={handleToggle}
+          className={`${styles.cursorPointer} d-flex align-items-center mt-1`}
+        >
+          
+        </motion.div>
+      </Col>
+    </Row>
+  );
+}
