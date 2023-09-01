47889877b636040c735cf4aa0b20db41ac4f3b23
priyans619 <priyansh.2b@gmail.com>
2023-09-01 21:01:33 +0530

    row and coloum for all countries

--- Components/i-components/Countries.jsx
+++ Components/i-components/Countries.jsx
@@ -1,4 +1,5 @@
-
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import { useRouter } from "next/router";
 
 
@@ -10,4 +11,35 @@ export default function Countries({countries}) {
       router.push('/' + countryCode)
   }
 
+  return (
+
+    <Row className={`${styles.countriesRow} mt-4 g-4`}>
+      {countries.map((country) => {
+        const id = uuidv4();
+        let { cca3, name } = country;
+        let {} = name;
+
+        return (
+          <Col
+            xs={{ span: 10, offset: 1 }}
+            md={{ span: 3, offset: 0 }}
+            key={id}
+          >
+            <motion.div
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 1.3 }}
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              viewport={{ once: true }}
+              transition={{ ease: "easeOut", duration: 0.3 }}
+              onClick={() => showDetailsHandler(cca3)}
+              className={`${styles.countryContainer} country-container rounded shadow-sm`}
+            >
+             
+            </motion.div>
+          </Col>
+        );
+      })}
+    </Row>
+  );
 }
