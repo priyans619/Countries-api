65b7d871eec7fe23b434d13b3c50ba4b04cb8e0a
priyans619 <priyansh.2b@gmail.com>
2023-09-01 21:09:03 +0530

    Flags and population section

--- Components/i-components/Countries.jsx
+++ Components/i-components/Countries.jsx
@@ -16,8 +16,8 @@ export default function Countries({countries}) {
     <Row className={`${styles.countriesRow} mt-4 g-4`}>
       {countries.map((country) => {
         const id = uuidv4();
-        let { cca3, name } = country;
-        let {} = name;
+        let { flags, cca3, name, population } = country;
+        let {common} = name;
 
         return (
           <Col
@@ -35,7 +35,45 @@ export default function Countries({countries}) {
               onClick={() => showDetailsHandler(cca3)}
               className={`${styles.countryContainer} country-container rounded shadow-sm`}
             >
-             
+              <motion.div
+                initial={{ opacity: 0, y: -50 }}
+                // animate={{ opacity: 1 }}
+                whileInView={{ y: 0, opacity: 1 }}
+                viewport={{ once: true }}
+                transition={{ ease: "easeOut", duration: 0.8, delay: 0.5 }}
+                className={`${styles.imgContainer}`}
+              >
+                <img
+                  src={flags.png}
+                  alt=""
+                  width="100%"
+                  height="100%"
+                  className={`${styles.img} rounded`}
+                />
+              </motion.div>
+
+              <div className="px-3 mt-3">
+                <motion.p
+                  initial={{ opacity: 0 }}
+                  whileInView={{ opacity: 1 }}
+                  transition={{ duration: 0.5, delay: 0.7 }}
+                  viewport={{ once: true }}
+                  className="country-key fw-bold"
+                >
+                  {common}
+                </motion.p>
+                <motion.p
+                  initial={{ opacity: 0 }}
+                  whileInView={{ opacity: 1 }}
+                  transition={{ duration: 0.5, delay: 0.9 }}
+                  viewport={{ once: true }}
+                  className="country-key fs-7 fw-bold"
+                >
+                  Population:{" "}
+                  <span className="fs-6 country-value">{population}</span>
+                </motion.p>
+                
+              </div>
             </motion.div>
           </Col>
         );
