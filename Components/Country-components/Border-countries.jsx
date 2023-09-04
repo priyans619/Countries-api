9de3a651ddbc63e48dc5eb8c1e1588f555539be6
priyans619 <priyansh.2b@gmail.com>
2023-09-04 21:01:58 +0530

    creatiing func for border counties

---
+++ Components/Country-components/Border-countries.jsx
@@ -0,0 +1,9 @@
+import { motion } from 'framer-motion';
+import { useRouter } from "next/router";
+
+export default function BorderCountries({borderCountries}) {
+    const router = useRouter();
+    const { borders } = borderCountries;
+
+    return ()
+}
