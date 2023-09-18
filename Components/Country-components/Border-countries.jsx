decdc66633599110346858698522c327fbfe267b
priyans619 <priyansh.2b@gmail.com>
2023-09-18 17:26:26 +0530

    Update border

--- Components/Country-components/Border-countries.jsx
+++ Components/Country-components/Border-countries.jsx
@@ -8,39 +8,47 @@ export default function BorderCountries({borderCountries}) {
     return (
         
     
-  <div className="pt-4">
-    <motion.p
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.5, delay: 4.4 }}
-      className={`${
-        borders && "d-flex flex-wrap align-items-center"
-      } country-key fs-7 fw-bold`}
-    >
-       {borders ? (
-            borders.map((border, index) => {
-              const id = uuidv4();
-              let delayValue = 4.4;
-              index = index + 1; 
-              delayValue = delayValue + 0.3 * index; 
-            
-              return (
-                <motion.span
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  whileTap={{ scale: 1.3 }}
-                  transition={{ duration: 0.5, delay: `${delayValue}` }}
-                  key={id}
-                  onClick={() => router.push(`/${border}`)}
-                  className={`${styles.cursorPointer} inline-block rounded fs-6 country-value border px-2 py-1 shadow-sm ms-1 my-1`}
-                >
-                  {border}
-                </motion.span>
-              );   
-            }  
-    </motion.p>
-    
-  </div>
-    );
+      <div className="pt-4">
+      <motion.p
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 4.4 }}
+        className={`${
+          borders && "d-flex flex-wrap align-items-center"
+        } country-key fs-7 fw-bold`}
+      >
+        Border Countries:
+        {borders ? (
+          borders.map((border, index) => {
+            const id = uuidv4();
+            let delayValue = 4.4; 
+            index = index + 1; 
+            delayValue = delayValue + 0.3 * index; 
 
+            return (
+              <motion.span
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                whileTap={{ scale: 1.3 }}
+                transition={{ duration: 0.5, delay: `${delayValue}` }}
+                key={id}
+                onClick={() => router.push(`/${border}`)}
+                className={`${styles.cursorPointer} inline-block rounded fs-6 country-value border px-2 py-1 shadow-sm ms-1 my-1`}
+              >
+                {border}
+              </motion.span>
+            );
+          })
+        ) : (
+          <motion.span
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            className="country-value fs-6 text-light-blue ms-1 fw-bold"
+          >
+            None
+          </motion.span>
+        )}
+      </motion.p>
+    </div>
+  );
 }
