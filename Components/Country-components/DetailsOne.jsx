c00e3e5fe633237df5ad05cab2b8c79975866125
priyans619 <priyansh.2b@gmail.com>
2023-09-20 18:45:53 +0530

    Update Native -Name

--- Components/Country-components/DetailsOne.jsx
+++ Components/Country-components/DetailsOne.jsx
@@ -12,8 +12,9 @@ export default function DetailsOne({detailsOne}) {
           animate={{ opacity: 1 }}
           transition={{ duration: 0.5, delay: 2.0 }}
           className="country-key fs-7 fw-bold"
-        >
-            <span className="fs-4 country-value">
+        >    
+            Native Name:{""}
+            <span className="fs-6 country-value">
             {nativeName ? nativeName : "No Native Name"}
             </span>
         </motion.p>
