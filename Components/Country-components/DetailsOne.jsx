42b67f554ea5acc5ab63ec548ddc473e4825f3e3
priyans619 <priyansh.2b@gmail.com>
2023-09-20 18:39:40 +0530

    inserting column for all details

--- Components/Country-components/DetailsOne.jsx
+++ Components/Country-components/DetailsOne.jsx
@@ -1,11 +1,30 @@
 import Col from "react-bootstrap//Col";
+import { motion } from 'framer-motion'
 import style from "styles/singlecountry.module.scss";
 
 export default function DetailsOne({detailsOne}) {
 
     const { nativeName, region, subregion, population, capital } = detailsOne;
     return (
+    <Col xs={12} md={6} className={`${style.descCol1}`}>
+      <motion.p
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 2.0 }}
+          className="country-key fs-7 fw-bold"
+        >
+            
+        </motion.p>
+        
 
-    );
+
+
+    </Col>
+    
+    
+    
+    
+    
+        );
 
 }
