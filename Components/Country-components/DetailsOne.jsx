3a3d2f1f30f6e3cb7067a26543a112dd9fb5d3c2
priyans619 <priyansh.2b@gmail.com>
2023-09-20 18:33:33 +0530

    Import module

--- Components/Country-components/DetailsOne.jsx
+++ Components/Country-components/DetailsOne.jsx
@@ -1,6 +1,11 @@
-
+import Col from "react-bootstrap//Col";
+import style from "styles/singlecountry.module.scss";
 
 export default function DetailsOne({detailsOne}) {
 
     const { nativeName, region, subregion, population, capital } = detailsOne;
-    
+    return (
+
+    );
+
+}
