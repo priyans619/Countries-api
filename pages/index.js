84ab30dd26e8b103098bb3ede00cff9b18d7bbf6
priyans619 <priyansh.2b@gmail.com>
2023-11-11 22:01:53 +0530

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,8 @@
+import { useState} from 'react'
 
+export default function Home({data}) {}
 const [countries, setCountries] = useState(data);
 const [value, setValue] = useState('');
+let regions = new Set(data.map(country => country.region))
+regions = [ ...regions].sort();
+regions = ['All', ...regions]
