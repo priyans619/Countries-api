61cb8440c0ee48be90be25dc8fd3840c2b665cc3
priyans619 <priyansh.2b@gmail.com>
2023-11-14 21:42:59 +0530

    adding useEffect

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import { useState} from 'react'
 
-export default function Home({data}) {}
+export default function Home({data}) {
+const isMounted = useRef(false);
+
 const [countries, setCountries] = useState(data);
 const [value, setValue] = useState('');
 let regions = new Set(data.map(country => country.region))
 regions = [ ...regions].sort();
 regions = ['All', ...regions]
+
+useEffect(() => {})
+    
+}
